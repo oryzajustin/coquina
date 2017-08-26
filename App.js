91304263e480f195d09f6735abcf17fd4839a2e2
Justin Koh <justinkoh@Justins-MacBook-Pro.local>
2017-08-26 14:10:39 -0400

    text box

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
+import { Constants } from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    inputValue: "Holy !"
+  };
+
+  _handleTextChange = inputValue => {
+    this.setState({ inputValue });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      
+        <Text style={styles.paragraph}>
+          Journal entry
+        </Text>
+        
+        <View style={styles.box}>
+        
+          <TextInput
+            value={this.state.inputValue}
+            onChangeText={this._handleTextChange}
+            style={{ width: 200, height: 44, padding: 8 }}
+          />
+        
+        </View>
+        
       </View>
+      
     );
   }
 }
@@ -14,8 +38,25 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+  },
+  box: {
+    width: 'auto',
+    height: 'auto',
+    backgroundColor: 'skyblue',
+    borderWidth: 2,
+    borderColor: 'steelblue',
+    borderRadius: 20,
   },
 });
+
