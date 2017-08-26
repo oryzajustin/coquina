009f3a851909504af6b4d07024b4629c4168a2f6
Justin Koh <justinkoh@Justins-MacBook-Pro.local>
2017-08-26 18:36:44 -0400

    bro this is lit

--- App.js
+++ App.js
@@ -1,36 +1,65 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet, TextInput } from 'react-native';
-import { Constants } from 'expo';
+import React from 'react';
+import {StyleSheet, Text, View, FlatList, TextInput, KeyboardAvoidingView, TouchableOpacity} from 'react-native';
+import {send, subscribe} from 'react-native-training-chat-server';
+import ReversedFlatList from 'react-native-reversed-flat-list';
+
+const NAME = '';
+const CHANNEL = 'pls';
 
 export default class App extends React.Component {
   state = {
-    inputValue: "Write something..."
+    typing: "",
+    messages: [],
   };
 
-  _handleTextChange = inputValue => {
-    this.setState({ inputValue });
-  };
+  componentWillMount() {
+    subscribe(CHANNEL, messages => {
+      this.setState({messages});
+    });
+  }
+
+  async sendMessage() {
+  //send message with user name but wait until sent to display
+    await send({
+    channel: CHANNEL,
+    sender: NAME,
+    message: this.state.typing
+  });
+
+  //clear text input after sent
+  this.setState({
+    typing: '',
+  });
+}
+
+  renderItem({item}) {
+    return (
+      <View style={styles.row}>
+        <Text style={styles.sender}>{item.sender}</Text>
+        <Text style={styles.message}>{item.message}</Text>
+      </View>
+    );
+  }
 
   render() {
     return (
       <View style={styles.container}>
-      
-        <Text style={styles.paragraph}>
-          Journal Entry
-        </Text>
-        
-        <View style={styles.box}>
-        
-          <TextInput
-            value={this.state.inputValue}
-            onChangeText={this._handleTextChange}
-            style={{ width: 200, height: 44, padding: 8 }}
-          />
-        
+        <ReversedFlatList data={this.state.messages} renderItem={this.renderItem} />
+          <KeyboardAvoidingView behavior="padding">
+          <View style={styles.footer}>
+           <TextInput
+              value={this.state.typing}
+              onChangeText={text => this.setState({typing: text})}
+              style={styles.input}
+              underlineColorAndroid="transparent"
+              placeholder="Type something nice"
+            />
+              <TouchableOpacity onPress={this.sendMessage.bind(this)}>
+               <Text style={styles.send}>Send</Text>
+                </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView> 
         </View>
-        
-      </View>
-      
     );
   }
 }
@@ -38,25 +67,35 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: '#fff',
   },
-  paragraph: {
-    margin: 24,
+  row: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+  },
+  message: {
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#34495e',
   },
-  box: {
-    width: 'auto',
-    height: 'auto',
-    backgroundColor: 'skyblue',
-    borderWidth: 2,
-    borderColor: 'steelblue',
-    borderRadius: 20,
+  sender: {
+    fontWeight: 'bold',
+    paddingRight: 10,
+  },
+  footer: {
+    flexDirection: 'row',
+    backgroundColor: '#eee',
+  },
+  input: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    fontSize: 18,
+    flex: 1,
+  },
+  send: {
+    alignSelf: 'center',
+    color: 'lightseagreen',
+    fontSize: 16,
+    fontWeight: 'bold',
+    padding: 20,
   },
 });
-
