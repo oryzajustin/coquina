68bdc912f9b91c136a0257b0bf0129b6e42aff8e
VictoryShoe <victoryshoe@hotmail.com>
2017-08-27 02:22:56 -0400

    My man!

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View, FlatList, TextInput, KeyboardAvoidingView, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, FlatList, TextInput, KeyboardAvoidingView, TouchableOpacity, Image} from 'react-native';
 import {send, subscribe} from 'react-native-training-chat-server';
 import ReversedFlatList from 'react-native-reversed-flat-list';
 import Header from './Header';
@@ -9,8 +9,11 @@ const DATE = new Date;
 const DATESTRING = DATE.toDateString();
 const NAME = '';
 const CHANNEL = DATESTRING;
+const ME_AVATAR = 'https://i.ytimg.com/vi/Q4ZIKzRSc0s/hqdefault.jpg';
+const BOT_AVATAR = 'https://i.ytimg.com/vi/I04OIfbBrTg/hqdefault.jpg?sqp=-oaymwEWCKgBEF5IWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLCAIAqgbtPKAQQS1mHdmefcigLktg';
 
 export default class App extends React.Component {
+
   state = {
     typing: "",
     messages: [],
@@ -23,24 +26,38 @@ export default class App extends React.Component {
   }
 
   async sendMessage() {
-  //send message with user name but wait until sent to display
+    //send message with user name but wait until sent to display
     await send({
-    channel: CHANNEL,
-    sender: NAME,
-    message: this.state.typing
-  });
+      channel: CHANNEL,
+      sender: NAME,
+      message: this.state.typing,
+      avatar: ME_AVATAR,
+    });
 
-  //clear text input after sent
-  this.setState({
-    typing: '',
-  });
-}
+    await send({
+      channel: CHANNEL,
+      sender: 'Bot',
+      message: 'My man!',
+      avatar: BOT_AVATAR,
+    });
+
+    //clear text input after sent
+    this.setState({
+      typing: '',
+    });
+  }
 
   renderItem({item}) {
     return (
       <View style={styles.row}>
-        <Text style={styles.sender}>{item.sender}</Text>
-        <Text style={styles.message}>{item.message}</Text>
+        <Image
+          style={styles.avatar}
+          source={{uri: item.avatar}}
+        />
+        <View style={styles.rowText}>
+          <Text style={styles.sender}>{item.sender}</Text>
+          <Text style={styles.message}>{item.message}</Text>
+        </View>
       </View>
     );
   }
@@ -49,22 +66,24 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Header title={CHANNEL} />
+
         <ReversedFlatList data={this.state.messages} renderItem={this.renderItem} />
-          <KeyboardAvoidingView behavior="padding">
+
+        <KeyboardAvoidingView behavior="padding">
           <View style={styles.footer}>
-           <TextInput
+            <TextInput
               value={this.state.typing}
               onChangeText={text => this.setState({typing: text})}
               style={styles.input}
               underlineColorAndroid="transparent"
               placeholder="Write something here..."
             />
-              <TouchableOpacity onPress={this.sendMessage.bind(this)}>
-               <Text style={styles.send}>Send</Text>
-                </TouchableOpacity>
+            <TouchableOpacity onPress={this.sendMessage.bind(this)}>
+              <Text style={styles.send}>Send</Text>
+            </TouchableOpacity>
           </View>
         </KeyboardAvoidingView> 
-        </View>
+      </View>
     );
   }
 }
@@ -102,5 +121,14 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     padding: 20,
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginRight: 10,
+  },
+  rowText: {
+    flex: 1,
   },
 });
